3b1dce1a158c92b970f8f818b1f97dfb22ceec4d
alvesjaov <victoralves169@outlook.com>
2023-10-03 09:20:44 -0300

    feat: adiciona tratamento para caso não haja doações no histórico de doações

--- src/models/Donor.js
+++ src/models/Donor.js
@@ -12,15 +12,19 @@ const donorSchema = mongoose.Schema({
   expiryDate: { type: String }
 });
 
-donorSchema.pre('save', function(next) {
-  // Adiciona a data atual ao histórico de doações
-  const currentDate = formatDate(new Date());
-  // Atualiza donationDate e expiryDate
-  this.donationDate = currentDate;
-  this.expiryDate = formatDate(new Date(Date.now() + 1*24*60*60*1000));
+donorSchema.pre('save', function (next) {
+  // Adiciona a data atual ao histórico de doações se houver alguma doação
+  if (this.donationHistory.length > 0) {
+    const currentDate = formatDate(new Date());
+    // Atualiza donationDate e expiryDate
+    this.donationDate = currentDate;
+    this.expiryDate = formatDate(new Date(Date.now() + 1 * 24 * 60 * 60 * 1000));
+  } else {
+    this.donationDate = 'Não há doações';
+    this.expiryDate = 'Não há doações';
+  }
 
   next();
 });
-
 
 export default mongoose.model('Donor', donorSchema);
