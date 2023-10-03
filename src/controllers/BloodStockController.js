522b21761ddfc38e1927f860949a040664ab0369
alvesjaov <victoralves169@outlook.com>
2023-10-03 09:14:47 -0300

    refactor: altera lógica de agrupamento para contar as doações em vez de doadores no estoque

--- src/controllers/BloodStockController.js
+++ src/controllers/BloodStockController.js
@@ -1,5 +1,5 @@
 import Donor from '../models/Donor.js';
-import Stock from '../models/Stock.js'; 
+import Stock from '../models/Stock.js';
 
 // Função assíncrona para contar e atualizar os tipos sanguíneos
 async function countAndUpdateBloodTypes(request, response) {
@@ -7,12 +7,13 @@ async function countAndUpdateBloodTypes(request, response) {
         // Pega a data atual em formato ISO
         const currentDate = new Date().toISOString();
 
-        // Conta o número de doadores para cada tipo sanguíneo cuja data de validade ainda não passou
+        // Conta o número de doações para cada tipo sanguíneo cuja data de validade ainda não passou
         const bloodTypeCounts = await Donor.aggregate([
             { $match: { expiryDate: { $gte: currentDate } } }, // Filtra os doadores cuja data de validade ainda não passou
-            { $group: { _id: '$bloodType', count: { $sum: 1 } } }, // Agrupa os doadores por tipo sanguíneo e conta o número de doadores para cada grupo
+            { $group: { _id: '$bloodType', count: { $sum: { $size: "$donationHistory" } } } }, // Agrupa os doadores por tipo sanguíneo e conta o número de doações para cada grupo
             { $sort: { _id: 1 } } // Ordena os resultados por tipo sanguíneo em ordem ascendente
         ]);
+
 
         // Cria um objeto com contagens para todos os tipos sanguíneos
         const allBloodTypes = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
@@ -23,7 +24,7 @@ async function countAndUpdateBloodTypes(request, response) {
         });
 
         // Atualiza as contagens no banco de dados
-        await Stock.updateOne({}, {$set:allCounts}, { upsert: true }); // Atualiza um documento na coleção Stock com as novas contagens. Se nenhum documento existir, cria um novo (upsert: true).
+        await Stock.updateOne({}, { $set: allCounts }, { upsert: true }); // Atualiza um documento na coleção Stock com as novas contagens. Se nenhum documento existir, cria um novo (upsert: true).
 
         // Envia uma resposta com status 200 (OK) e as contagens como corpo da resposta
         response.status(200).json(allCounts);
@@ -35,4 +36,4 @@ async function countAndUpdateBloodTypes(request, response) {
 }
 
 // Exporta a função countAndUpdateBloodTypes
-export { countAndUpdateBloodTypes };
+export default countAndUpdateBloodTypes ;
