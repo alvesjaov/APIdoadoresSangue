f93ffb417ab83736eee866289de496b70c60fc29
alvesjaov <victoralves169@outlook.com>
2023-10-03 23:03:04 -0300

    refactor: altera lógica de agrupamento do array para contar as doações

--- src/controllers/BloodStockController.js
+++ src/controllers/BloodStockController.js
@@ -1,39 +1,48 @@
+// Importa os modelos Donor e Stock
 import Donor from '../models/Donor.js';
 import Stock from '../models/Stock.js';
 
 // Função assíncrona para contar e atualizar os tipos sanguíneos
 async function countAndUpdateBloodTypes(request, response) {
     try {
-        // Pega a data atual em formato ISO
-        const currentDate = new Date().toISOString();
+        // Pega a data atual e converte para uma string no formato "AAAA-MM-DD"
+        const currentDate = new Date().toISOString().split('T')[0];
 
-        // Conta o número de doações para cada tipo sanguíneo cuja data de validade ainda não passou
+        // Realiza uma operação de agregação no modelo Donor
         const bloodTypeCounts = await Donor.aggregate([
-            { $match: { expiryDate: { $gte: currentDate } } }, // Filtra os doadores cuja data de validade ainda não passou
-            { $group: { _id: '$bloodType', count: { $sum: { $size: "$donationHistory" } } } }, // Agrupa os doadores por tipo sanguíneo e conta o número de doações para cada grupo
-            { $sort: { _id: 1 } } // Ordena os resultados por tipo sanguíneo em ordem ascendente
+            // Adiciona um novo campo expiryDateWithoutTimezone que contém apenas a parte da data de expiryDate
+            { $addFields: { expiryDateWithoutTimezone: { $substr: ["$expiryDate", 0, 10] } } },
+            // Filtra os documentos onde expiryDateWithoutTimezone é maior ou igual à currentDate
+            { $match: { expiryDateWithoutTimezone: { $gte: currentDate } } },
+            // Desagrupa o array donationHistory para que cada doação seja uma entrada separada
+            { $unwind: '$donationHistory' },
+            // Agrupa os documentos por tipo sanguíneo e conta o número de doações para cada grupo
+            { $group: { _id: '$bloodType', count: { $sum: 1 } } },
+            // Ordena os resultados por tipo sanguíneo em ordem ascendente
+            { $sort: { _id: 1 } }
         ]);
 
-
-        // Cria um objeto com contagens para todos os tipos sanguíneos
+        // Define todos os tipos sanguíneos possíveis
         const allBloodTypes = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
         const allCounts = {};
+        // Para cada tipo sanguíneo, encontra a contagem correspondente nos resultados da agregação
         allBloodTypes.forEach(type => {
-            const found = bloodTypeCounts.find(count => count._id === type); // Procura a contagem para o tipo sanguíneo atual
-            allCounts[type] = found ? found.count : 0; // Se a contagem foi encontrada, usa ela. Caso contrário, usa 0.
+            const found = bloodTypeCounts.find(count => count._id === type);
+            // Se a contagem foi encontrada, usa ela. Caso contrário, usa 0.
+            allCounts[type] = found ? found.count : 0;
         });
 
-        // Atualiza as contagens no banco de dados
-        await Stock.updateOne({}, { $set: allCounts }, { upsert: true }); // Atualiza um documento na coleção Stock com as novas contagens. Se nenhum documento existir, cria um novo (upsert: true).
+        // Atualiza as contagens no modelo Stock. Se nenhum documento existir, cria um novo (upsert: true).
+        await Stock.updateOne({}, { $set: allCounts }, { upsert: true });
 
         // Envia uma resposta com status 200 (OK) e as contagens como corpo da resposta
         response.status(200).json(allCounts);
     } catch (error) {
-        // Se ocorrer algum erro, envia uma resposta com status 500 (Erro Interno do Servidor) e uma mensagem de erro.
+        // Se ocorrer algum erro, registra a mensagem de erro no console e envia uma resposta com status 500 (Erro Interno do Servidor)
         console.log(error.message)
         response.status(500).send('Ocorreu um erro ao contar e atualizar os tipos sanguíneos. Por favor, tente novamente.');
     }
 }
 
 // Exporta a função countAndUpdateBloodTypes
-export default countAndUpdateBloodTypes ;
+export default countAndUpdateBloodTypes;
