0b1f890b7f05bc1e1aab7fbfb5e266a128c7560b
alvesjaov <victoralves169@outlook.com>
2023-10-05 14:36:52 -0300

    refactor: countAndUpdateBloodTypes para funções menores

--- src/controllers/BloodStockController.js
+++ src/controllers/BloodStockController.js
@@ -1,60 +1,72 @@
-
 import Donor from '../models/Donor.js';
 import Stock from '../models/Stock.js';
 
-// Função assíncrona para contar e atualizar os tipos sanguíneos
+// Função para remover doações expiradas
+async function removeExpiredDonations(currentDate) {
+    // Atualiza todos os documentos Donor onde a data de validade é menor que a data atual
+    // A operação $pull remove itens do array donationHistory que correspondem à condição especificada
+    await Donor.updateMany(
+        { 'donationHistory.expiryDate': { $lt: currentDate } },
+        { $pull: { donationHistory: { expiryDate: { $lt: currentDate } } } }
+    );
+}
+
+// Função para contar os tipos sanguíneos
+async function countBloodTypes(currentDate) {
+    // Realiza uma operação de agregação no modelo Donor
+    const bloodTypeCounts = await Donor.aggregate([
+        // Desagrupa o array donationHistory para que cada doação seja uma entrada separada
+        { $unwind: '$donationHistory' },
+        // Filtra os documentos onde a data de validade da doação é maior ou igual à data atual
+        { $match: { 'donationHistory': { $gte: currentDate } } },
+        // Agrupa os documentos por tipo sanguíneo e conta o número de doações para cada grupo
+        { $group: { _id: '$bloodType', count: { $sum: 1 } } },
+        // Ordena os resultados por tipo sanguíneo em ordem ascendente
+        { $sort: { _id: 1 } }
+    ]);
+    return bloodTypeCounts;
+}
+
+// Função para preparar a contagem de todos os tipos sanguíneos
+function prepareAllCounts(bloodTypeCounts) {
+    const allBloodTypes = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+    let allCounts = {};
+    allBloodTypes.forEach(type => {
+        allCounts[type] = 0;
+    });
+
+    allBloodTypes.forEach(type => {
+        const found = bloodTypeCounts.find(count => count._id === type);
+        allCounts[type] = found ? found.count : 0;
+    });
+
+    return allCounts;
+}
+
+// Função para atualizar o estoque com as contagens
+async function updateStock(allCounts) {
+    await Stock.updateOne({}, { $set: allCounts }, { upsert: true });
+}
+
+// Função principal para contar e atualizar os tipos sanguíneos
 async function countAndUpdateBloodTypes(request, response) {
     try {
-        // Pega a data atual e converte para uma string no formato "AAAA-MM-DD"
         const currentDate = new Date().toISOString().split('T')[0];
 
-        // Primeiro, exclua todas as doações que já expiraram
-        // Isso é feito atualizando todos os documentos Donor onde a data de validade é menor que a data atual
-        // A operação $pull remove itens do array donationHistory que correspondem à condição especificada
-        await Donor.updateMany(
-            { 'donationHistory.expiryDate': { $lt: currentDate } },
-            { $pull: { donationHistory: { expiryDate: { $lt: currentDate } } } }
-        );
-        console.log('Doações expiradas removidas.', currentDate);
+        await removeExpiredDonations(currentDate);
 
-        // Realiza uma operação de agregação no modelo Donor
-        const bloodTypeCounts = await Donor.aggregate([
-            // Desagrupa o array donationHistory para que cada doação seja uma entrada separada
-            { $unwind: '$donationHistory' },
-            // Filtra os documentos onde a data de validade da doação é maior ou igual à data atual
-            { $match: { 'donationHistory': { $gte: currentDate } } },
-            // Agrupa os documentos por tipo sanguíneo e conta o número de doações para cada grupo
-            { $group: { _id: '$bloodType', count: { $sum: 1 } } },
-            // Ordena os resultados por tipo sanguíneo em ordem ascendente
-            { $sort: { _id: 1 } }
-        ]);
-        console.log('Contagem de tipos sanguíneos realizada.', bloodTypeCounts);
+        const bloodTypeCounts = await countBloodTypes(currentDate);
 
-        // Define todos os tipos sanguíneos possíveis
-        const allBloodTypes = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
-        let allCounts = {};
-        allBloodTypes.forEach(type => {
-            allCounts[type] = 0;
-        });
+        const allCounts = prepareAllCounts(bloodTypeCounts);
 
-        // Para cada tipo sanguíneo, encontra a contagem correspondente nos resultados da agregação
-        allBloodTypes.forEach(type => {
-            const found = bloodTypeCounts.find(count => count._id === type);
-            // Se a contagem foi encontrada, usa ela. Caso contrário, usa 0.
-            allCounts[type] = found ? found.count : 0;
-        });
+        await updateStock(allCounts);
 
-        // Atualiza as contagens no modelo Stock. Se nenhum documento existir, cria um novo (upsert: true).
-        await Stock.updateOne({}, { $set: allCounts }, { upsert: true });
-
-        // Envia uma resposta com status 200 (OK) e as contagens como corpo da resposta
         response.status(200).json(allCounts);
     } catch (error) {
-        // Se ocorrer algum erro, registra a mensagem de erro no console e envia uma resposta com status 500 (Erro Interno do Servidor)
         console.log(error.message)
         response.status(500).send('Ocorreu um erro ao contar e atualizar os tipos sanguíneos. Por favor, tente novamente.');
     }
 }
 
-// Exporta a função countAndUpdateBloodTypes
+// Exportando a função countAndUpdateBloodTypes
 export default countAndUpdateBloodTypes;
