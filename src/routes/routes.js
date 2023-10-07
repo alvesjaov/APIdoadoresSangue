6bf2aa29256eee041b07669b4e2c8b07f916b5f0
alvesjaov <victoralves169@outlook.com>
2023-10-07 18:53:43 -0300

    refactor: ajustei o código para fácil intendimento

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-import { createDonor, getDonors, createDonation, updateDonor, deleteDonor, deleteLastDonation } from '../controllers/DonorController.js';
-import countAndUpdateBloodTypes from '../controllers/BloodStockController.js';
+import { createDonor, getDonors, updateDonor, deleteDonor, createDonation, deleteLastDonation } from '../controllers/DonorController.js';
+import  countAndUpdateBloodTypes from '../controllers/BloodStockController.js';
 
 const routes = Router();
 
 routes.post('/donors', createDonor);
 routes.get('/donors/:id?', getDonors); // O id é opcional
-routes.post('/donors/:id/donation', createDonation);
 routes.patch('/donors/:id', updateDonor);
 routes.delete('/donors/:id', deleteDonor);
-routes.get('/stock', countAndUpdateBloodTypes);
+routes.post('/donors/:id/donation', createDonation);
 routes.delete('/donors/:id/donation', deleteLastDonation);
+routes.get('/stock', countAndUpdateBloodTypes);
 
 export default routes;
