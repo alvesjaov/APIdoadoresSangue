cb6723f932a421999ac9076dafdab3104da4ef83
alvesjaov <victoralves169@outlook.com>
2023-10-19 03:51:51 -0300

    feat: Adiciona rotas de funcionários protegidas pelo middleware verifyAdmin

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,17 +2,25 @@ import { Router } from 'express';
 import { createDonor, getDonors, updateDonor, deleteDonor } from '../controllers/DonorController.js';
 import { createDonation, addBloodExams, deleteLastDonation } from '../controllers/DonationController.js';
 import countAndUpdateBloodTypes from '../controllers/BloodStockController.js';
+import { createEmployee, readEmployee, updateEmployeePassword, deleteEmployee } from '../controllers/EmployeeController.js'; // Importe a função createEmployee
+import verifyAdmin from '../middleware/VerifyAdmin.js';
 
 const routes = Router();
 
-routes.post('/donors', createDonor);
+routes.post('/donors', createDonor); // Rota para criar um doador
 routes.get('/donors/:id?', getDonors); // O id é opcional
-routes.patch('/donors/:id', updateDonor);
-routes.delete('/donors/:id', deleteDonor);
-routes.post('/donors/:id/donation', createDonation);
-routes.delete('/donors/:id/donation', deleteLastDonation);
-routes.patch('/donors/:id/exams', addBloodExams);
-routes.get('/stock', countAndUpdateBloodTypes);
+routes.patch('/donors/:id', updateDonor); // Rota para atualizar um doador
+routes.delete('/donors/:id', deleteDonor); // Rota para deletar um doador
 
+routes.post('/donors/:id/donation', createDonation); // Rota para criar uma doação
+routes.post('/donors/:id/exams', addBloodExams); // Rota para adicionar exames de sangue
+routes.delete('/donors/:id/donation', deleteLastDonation); // Rota para deletar a última doação
+
+routes.get('/stock', countAndUpdateBloodTypes); // Rota para atualizar o estoque de sangue
+
+routes.post('/employees', verifyAdmin, createEmployee); // Rota para criar um funcionário
+routes.get('/employees/:code?', verifyAdmin, readEmployee); // Rota para ler funcionários. O código do funcionário é opcional
+routes.patch('/employees/:code/password', verifyAdmin, updateEmployeePassword); // Rota para alterar a senha de um funcionário
+routes.delete('/employees/:id', verifyAdmin, deleteEmployee); // Rota para deletar um funcionário
 
 export default routes;
