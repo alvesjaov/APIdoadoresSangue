b56e184773a34ddcdf230bba1c737e90d30f0fe2
alvesjaov <victoralves169@outlook.com>
2023-10-24 20:59:19 -0300

    feat: Separar rotas em arquivos individuais
    - Criei arquivos de rota individuais para doadores, doações, estoque e funcionários

---
+++ src/routes/donorRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { createDonor, getDonors, updateDonor, deleteDonor } from '../controllers/DonorController.js';
+import { verifyEmployee } from '../middleware/Verify.js';
+
+const donorRoutes = Router();
+
+donorRoutes.post('', verifyEmployee, createDonor); // Rota para criar um doador
+donorRoutes.get('/:id?', verifyEmployee, getDonors); // O id é opcional
+donorRoutes.patch('/:id', verifyEmployee, updateDonor); // Rota para atualizar um doador
+donorRoutes.delete('/:id', verifyEmployee, deleteDonor); // Rota para deletar um doador
+
+export default donorRoutes;
