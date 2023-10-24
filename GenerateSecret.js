bea4229021e6cf3fa1a5136a4ee5111748c49abf
alvesjaov <victoralves169@outlook.com>
2023-10-24 20:12:31 -0300

    refactor: simplificar script de geração de chave secreta

--- GenerateSecret.js
+++ GenerateSecret.js
@@ -1,26 +1,17 @@
 // no terminal node generateSecret.js
-
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
 import fs from 'fs';
 import crypto from 'crypto';
 
 // Gera uma chave secreta aleatória
 const secret = crypto.randomBytes(64).toString('hex');
 
-// Obtém o diretório do módulo atual
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// Define o caminho completo para o arquivo .env
-const envPath = resolve(__dirname, '../../.env');
-
 // Verifica se o arquivo .env existe, se não, cria um
-if (!fs.existsSync(envPath)) {
-  fs.writeFileSync(envPath, '');
+if (!fs.existsSync('.env')) {
+  fs.writeFileSync('.env', '');
 }
 
 // Lê o conteúdo do arquivo .env
-let envContent = fs.readFileSync(envPath, 'utf8');
+let envContent = fs.readFileSync('.env', 'utf8');
 
 // Verifica se JWT_SECRET já existe no arquivo .env
 let lines = envContent.split('\n');
@@ -35,5 +26,5 @@ if (index !== -1) {
 }
 
 // Escreve o conteúdo atualizado de volta para o arquivo .env
-fs.writeFileSync(envPath, lines.join('\n'));
+fs.writeFileSync('.env', lines.join('\n'));
 console.log('Chave secreta gerada com sucesso!');
