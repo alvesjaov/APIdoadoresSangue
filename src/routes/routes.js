fc3cdcff744efa833bf1994524524dd8d86a128c
alvesjaov <victoralves169@outlook.com>
2023-10-24 17:16:00 -0300

    feat: Adiciona autenticação de funcionário nas rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,23 +1,23 @@
 import { Router } from 'express';
 import { createDonor, getDonors, updateDonor, deleteDonor } from '../controllers/DonorController.js';
-import { createDonation,readDonation, addBloodExams, deleteLastDonation } from '../controllers/DonationController.js';
+import { createDonation, readDonation, addBloodExams, deleteLastDonation } from '../controllers/DonationController.js';
 import countAndUpdateBloodTypes from '../controllers/BloodStockController.js';
 import { createEmployee, readEmployee, updateEmployeePassword, deleteEmployee, loginEmployee } from '../controllers/EmployeeController.js'; // Importe a função loginEmployee
-import verifyAdmin from '../middleware/VerifyAdmin.js';
+import { verifyAdmin, verifyEmployee } from '../middleware/Verify.js';
 
 const routes = Router();
 
-routes.post('/donors', createDonor); // Rota para criar um doador
-routes.get('/donors/:id?', getDonors); // O id é opcional
-routes.patch('/donors/:id', updateDonor); // Rota para atualizar um doador
-routes.delete('/donors/:id', deleteDonor); // Rota para deletar um doador
+routes.post('/donors', verifyEmployee, createDonor); // Rota para criar um doador
+routes.get('/donors/:id?', verifyEmployee, getDonors); // O id é opcional
+routes.patch('/donors/:id', verifyEmployee, updateDonor); // Rota para atualizar um doador
+routes.delete('/donors/:id', verifyEmployee, deleteDonor); // Rota para deletar um doador
 
-routes.post('/donors/:id/donation', createDonation); // Rota para criar uma doação
-routes.get('/donors/donation/:id', readDonation); // Rota para ler uma doação específica
-routes.post('/donors/:id/exams', addBloodExams); // Rota para adicionar exames de sangue
-routes.delete('/donors/:id/donation', deleteLastDonation); // Rota para deletar a última doação
+routes.post('/donors/:id/donation', verifyEmployee, createDonation); // Rota para criar uma doação
+routes.get('/donors/donation/:id', verifyEmployee, readDonation); // Rota para ler uma doação específica
+routes.post('/donors/:id/exams', verifyEmployee, addBloodExams); // Rota para adicionar exames de sangue
+routes.delete('/donors/:id/donation', verifyEmployee, deleteLastDonation); // Rota para deletar a última doação
 
-routes.get('/stock', countAndUpdateBloodTypes); // Rota para atualizar o estoque de sangue
+routes.get('/stock', verifyEmployee, countAndUpdateBloodTypes); // Rota para atualizar o estoque de sangue
 
 routes.post('/employees', verifyAdmin, createEmployee); // Rota para criar um funcionário
 routes.get('/employees/:code?', verifyAdmin, readEmployee); // Rota para ler funcionários. O código do funcionário é opcional
