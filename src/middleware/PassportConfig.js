056801bec36142b6a7ad30878099e29dc7a1049c
alvesjaov <victoralves169@outlook.com>
2023-10-24 17:18:05 -0300

    feat: Adiciona estratégia de autenticação para funcionários
    - Adiciona nova estratégia 'jwt-employee' para autenticar um usuário como funcionário
    - Modifica a estratégia 'jwt' existente para verificar se o usuário é um administrador

--- src/middleware/PassportConfig.js
+++ src/middleware/PassportConfig.js
@@ -1,34 +1,53 @@
+// Importando os módulos necessários
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import Employee from '../models/Employee.js'; // Importa o modelo Employee
 
-// Função assíncrona para configurar o PassportConfig.js
+// Função assíncrona para configurar o passport
 async function configurePassport() {
-  // Definindo as opções para a estratégia JWT
+  // Opções para a estratégia JWT
   const opts = {
-    // Função para extrair o token JWT do cabeçalho de autorização
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    // Chave secreta para verificar a assinatura do token JWT
-    secretOrKey: process.env.JWT_SECRET
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Extrai o JWT do cabeçalho da requisição
+    secretOrKey: process.env.JWT_SECRET // Chave secreta para decodificar o JWT
   };
 
-  // Aplicando a estratégia JWT ao PassportConfig.js
-  passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+  // Configurando a estratégia 'jwt' do passport
+  passport.use('jwt', new JwtStrategy(opts, async (jwt_payload, done) => {
     try {
-      // Tentando encontrar um funcionário com o ID do payload do token JWT
+      // Procura um funcionário com o ID presente no payload do JWT
       const employee = await Employee.findById(jwt_payload?.id);
-      // Se o funcionário for encontrado e for um administrador, retorna o funcionário
+      // Se o funcionário for um administrador, retorna o funcionário
       if (employee?.isAdmin) {
         return done(null, employee);
       } else {
-        // Se o funcionário não for um administrador ou não for encontrado, retorna false
+        // Se o funcionário não for um administrador, retorna false
         return done(null, false);
       }
     } catch (err) {
-      // Se ocorrer um erro durante a busca do funcionário, retorna o erro
-      return done(err, false);
-    }
+      // Se houver um erro, retorna o erro
+      return done(err);
+    }    
   }));
+
+  // Configurando a estratégia 'jwt-employee' do passport
+  passport.use('jwt-employee', new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      // Procura um funcionário com o ID presente no payload do JWT
+      const employee = await Employee.findById(jwt_payload?.id);
+      // Se o funcionário existir, retorna o funcionário
+      if (employee) {
+        return done(null, employee);
+      } else {
+        // Se o funcionário não existir, retorna false
+        return done(null, false);
+      }
+    } catch (err) {
+      // Se houver um erro, retorna o erro
+      return done(err);
+    }    
+  }));
+
 }
 
+// Exporta a função configurePassport para ser usada em outros módulos
 export default configurePassport;
