4a34e0c4224081983553104a8215e6d8df2c2a75
alvesjaov <victoralves169@outlook.com>
2023-10-27 15:29:33 -0300

    refactor: Remover função de login do controller de funcionários

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -1,6 +1,5 @@
 import Employee from '../models/Employee.js'; // Importa o modelo Employee
 import bcrypt from 'bcrypt'; // Importa a biblioteca bcrypt para criptografia de senhas
-import jwt from 'jsonwebtoken'; // Importa a biblioteca jsonwebtoken para geração de tokens JWT
 
 // Função para gerar um código numérico aleatório para o funcionário
 function generateRandomEmployeeCode(length) {
@@ -126,33 +125,4 @@ async function deleteEmployee(request, response) {
   }
 }
 
-// Rota para login de um funcionário
-async function loginEmployee(request, response) {
-  const { employeeCode, password } = request.body; // Obtém o código do funcionário e a senha do corpo da requisição
-
-  try {
-    // Procura por um funcionário existente com o mesmo código de funcionário
-    const existingEmployee = await Employee.findOne({ employeeCode: employeeCode });
-
-    if (!existingEmployee) {
-      return response.status(400).json({ message: 'O código fornecido não pertence a nenhum funcionário.' });
-    }
-
-    // Verifica se a senha fornecida corresponde à senha do funcionário existente
-    const isPasswordValid = await bcrypt.compare(password, existingEmployee.password);
-
-    if (!isPasswordValid) {
-      return response.status(400).json({ message: 'Senha inválida.' });
-    }
-
-    // Se o código do funcionário e a senha estiverem corretos, gera um token JWT
-    const token = jwt.sign({ id: existingEmployee._id, isAdmin: existingEmployee.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    // Retorna uma mensagem de sucesso e o token JWT
-    response.json({ message: 'Login realizado com sucesso!', token });
-
-  } catch (error) {
-    response.status(500).json({ message: `Ocorreu um erro ao realizar o login. Por favor, tente novamente. Erro: ${error.message}` });
-  }
-}
-
-export { createEmployee, readEmployee, updateEmployeePassword, deleteEmployee, loginEmployee };
+export { createEmployee, readEmployee, updateEmployeePassword, deleteEmployee};
--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createEmployee, readEmployee, updateEmployeePassword, deleteEmployee, loginEmployee } from '../controllers/EmployeeController.js';
+import { createEmployee, readEmployee, updateEmployeePassword, deleteEmployee } from '../controllers/EmployeeController.js';
 import { verifyAdmin } from '../middleware/Verify.js';
 
 const employeeRoutes = Router();
@@ -8,6 +8,5 @@ employeeRoutes.post('/employees', verifyAdmin, createEmployee); // Rota para cri
 employeeRoutes.get('/employees/:code?', verifyAdmin, readEmployee); // Rota para ler funcionários. O código do funcionário é opcional
 employeeRoutes.patch('/employees/:code/password', verifyAdmin, updateEmployeePassword); // Rota para alterar a senha de um funcionário
 employeeRoutes.delete('/employees/:code', verifyAdmin, deleteEmployee); // Rota para deletar um funcionário
-employeeRoutes.post('/login', loginEmployee); // Rota para login de um funcionário
 
 export default employeeRoutes;
