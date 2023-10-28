1e657c4d86161bdd69b70f920ba933a05975b537
alvesjaov <victoralves169@outlook.com>
2023-10-27 22:09:47 -0300

    refactor: Movi generateSecret.js, swagger.json e vercel.json para pasta config

--- src/config/generateSecret.js
+++ src/config/generateSecret.js
@@ -2,17 +2,24 @@
 
 import fs from 'fs';
 import crypto from 'crypto';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Gera uma chave secreta aleatória
 const secret = crypto.randomBytes(64).toString('hex');
 
+// Define o caminho para o arquivo .env na raiz do projeto
+const envPath = path.join(__dirname, '../../.env');
+
 // Verifica se o arquivo .env existe, se não, cria um
-if (!fs.existsSync('.env')) {
-  fs.writeFileSync('.env', '');
+if (!fs.existsSync(envPath)) {
+  fs.writeFileSync(envPath, '');
 }
 
 // Lê o conteúdo do arquivo .env
-let envContent = fs.readFileSync('.env', 'utf8');
+let envContent = fs.readFileSync(envPath, 'utf8');
 
 // Verifica se JWT_SECRET já existe no arquivo .env
 let lines = envContent.split('\n');
@@ -27,5 +34,5 @@ if (index !== -1) {
 }
 
 // Escreve o conteúdo atualizado de volta para o arquivo .env
-fs.writeFileSync('.env', lines.join('\n'));
+fs.writeFileSync(envPath, lines.join('\n'));
 console.log('Chave secreta gerada com sucesso!');
