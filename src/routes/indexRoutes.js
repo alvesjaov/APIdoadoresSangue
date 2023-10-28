3670d380dece87a0183762e504b99291e7a01dd8
alvesjaov <victoralves169@outlook.com>
2023-10-27 22:54:21 -0300

    refactor: atualizar a implementação das rotas para usar o Router do Express

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,15 +1,16 @@
 // Importando rotas
+import { Router } from 'express';
 import loginRoutes from '../routes/loginRoutes.js';
 import employeeRoutes from '../routes/employeeRoutes.js';
 import donorRoutes from '../routes/donorRoutes.js';
 import donationRoutes from '../routes/donationRoutes.js';
 import stockRoutes from '../routes/stockRoutes.js';
 
-const routes = (app) => {
-    app.use(loginRoutes);
-    app.use(employeeRoutes);
-    app.use(donorRoutes);
-    app.use(donationRoutes);
-    app.use(stockRoutes);
-}
+const routes = Router();
+routes.use(loginRoutes);
+routes.use(employeeRoutes);
+routes.use(donorRoutes);
+routes.use(donationRoutes);
+routes.use(stockRoutes);
+
 export default routes; // Exportando rotas
