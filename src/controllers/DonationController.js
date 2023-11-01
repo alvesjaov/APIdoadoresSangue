cc8136d4b783c028b415e4dde63f7c2c63a607e2
alvesjaov <victoralves169@outlook.com>
2023-10-31 23:07:02 -0300

    refactor: permite a remoção de uma doação específica

--- src/controllers/DonationController.js
+++ src/controllers/DonationController.js
@@ -40,7 +40,7 @@ async function readDonation(request, response) {
         response.status(200).json(donation);
     } catch (error) {
         // Em caso de erro, retorna uma mensagem de erro
-        response.status(500).json({ error: `Ocorreu um erro ao ler a doação. Por favor, tente novamente. Erro: ${error.message}` });
+        response.status(500).json({ error: `Ocorreu um erro ao buscar a doação. Por favor, tente novamente Erro: ${error.message}` });
     }
 }
 
@@ -90,28 +90,34 @@ async function addBloodExams(request, response) {
     }
 }
 
-// Rota para deletar a última doação de um doador (DELETE)
-async function deleteLastDonation(request, response) {
-    const { id } = request.params; // Pega o id dos parâmetros da requisição
-    try {
-        // Procura pelo doador correspondente ao id fornecido
-        const donor = await Donor.findById(id);
-        if (!donor) {
-            response.status(404).json({ error: `ID ${id} não corresponde a nenhum doador` });
-        } else {
-            // Se o doador tiver histórico de doações, remove a última
-            if (donor.donationHistory.length > 0) {
-                donor.donationHistory.pop();
-                await donor.save(); // Salva o doador atualizado no banco de dados
+// Função assíncrona para deletar uma doação específica (DELETE)
+async function deleteDonation(request, response) {
+    const { id } = request.params; // Pega o id da doação dos parâmetros da requisição
 
-                response.status(200).json({ message: `Última doação removida para o doador com ID ${id}!` }); // Retorna sucesso se a última doação for removida corretamente
-            } else {
-                response.status(404).json({ error: `O doador com ID ${id} não tem histórico de doações.` }); // Retorna uma mensagem se o doador não tiver histórico de doações
-            }
+    try {
+        // Procura um doador que tenha uma doação com o ID especificado
+        const donor = await Donor.findOne({ "donationHistory._id": id });
+
+        // Se o doador for encontrado, recupera a doação do histórico de doações do doador
+        const donation = donor ? donor.donationHistory.id(id) : null;
+
+        // Se o doador ou a doação não forem encontrados, retorna uma mensagem de erro
+        if (!donor || !donation) {
+            return response.status(404).json({ error: `Doação não encontrada` });
         }
+
+        // Remove a doação específica
+        donor.donationHistory.pull(id);
+
+        // Salva o documento doador atualizado no banco de dados
+        await donor.save();
+
+        // Retorna uma resposta indicando que a doação foi deletada com sucesso
+        response.status(200).json({ message: `Doação com ID ${id} deletada com sucesso!` });
     } catch (error) {
-        response.status(500).json({ error: `Ocorreu um erro ao remover a última doação do doador com ID ${id}. Por favor, tente novamente. Erro: ${error.message}` });
+        // Em caso de erro, retorna uma mensagem de erro
+        response.status(500).json({ error: `Ocorreu um erro ao deletar a doação com ID ${id}. Por favor, tente novamente. Erro: ${error.message}` });
     }
 }
 
-export { createDonation, readDonation, addBloodExams, deleteLastDonation } // Exporta as funções para serem usadas em outros arquivos
+export { createDonation, readDonation, addBloodExams, deleteDonation } // Exporta as funções para serem usadas em outros arquivos
