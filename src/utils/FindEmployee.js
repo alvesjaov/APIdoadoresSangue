2aaed9866d4cca590336a809f74cd78c809d0985
João Victor Alves <victoralves169@outlook.com>
2023-12-04 00:00:26 -0300

    Fix: bug em FindEmployee.js

--- src/utils/FindEmployee.js
+++ src/utils/FindEmployee.js
@@ -1,21 +1,24 @@
+// Importe o modelo Employee se ainda não estiver importado
 import Employee from '../models/Employee.js';
-import mongoose from 'mongoose';
 
-// Função auxiliar para buscar funcionário por código ou nome
 async function findEmployeeByCodeOrName(codeOrName) {
-    let employee;
-    
-    // Primeiro, tente buscar pelo código
-    if (codeOrName.length === 6) {
-        employee = await Employee.findOne({ employeeCode: Number(codeOrName) });
+    try {
+        // Verifica se é um código
+        const isCode = !isNaN(codeOrName);
+
+        if (isCode) {
+            // Se for um código, busca pelo código
+            return await Employee.findOne({ code: codeOrName });
+        } else {
+            // Se for um nome, constrói uma expressão regular incremental
+            const regexString = `^${codeOrName}`;
+            const regex = new RegExp(regexString, 'i');
+            return await Employee.find({ name: regex });
+        }
+    } catch (error) {
+        console.log(error.message);
+        throw new Error('Erro ao buscar funcionários. Tente novamente.');
     }
-    
-    // Se nenhum funcionário foi encontrado, tente buscar pelo nome
-    if (!employee) {
-        employee = await Employee.findOne({ name: { $regex: '^' + codeOrName, $options: 'i' } });
-    }
-    
-    return employee;
 }
 
 export { findEmployeeByCodeOrName };
