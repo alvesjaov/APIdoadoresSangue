560c60818adc87610eeeb1bef7eedb6eb7902c1d
João Victor Alves <victoralves169@outlook.com>
2023-12-05 17:56:02 -0300

    Update: autorização nas rotas de doações e doadores

--- src/routes/donationRoutes.js
+++ src/routes/donationRoutes.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { createDonation, readDonation, addBloodExams, deleteDonation } from '../controllers/DonationController.js';
-import { verifyEmployee } from '../middleware/VerifyEmployees.js';
+import { verifyAdmin, verifyEmployee } from '../middleware/VerifyEmployees.js';
 import { checkBlacklistedToken } from '../middleware/Blacklist.js';
 
 
 const donationRoutes = Router();
 
-donationRoutes.post('/donations/:id', verifyEmployee, checkBlacklistedToken, createDonation); // Rota para criar uma doação
-donationRoutes.get('/donations/:id', verifyEmployee, checkBlacklistedToken, readDonation); // Rota para ler uma doação específica
-donationRoutes.patch('/donations/:id', verifyEmployee, checkBlacklistedToken, addBloodExams); // Rota para adicionar exames de sangue
-donationRoutes.delete('/donations/:id', verifyEmployee, checkBlacklistedToken, deleteDonation); // Rota para deletar a última doação
+donationRoutes.post('/donations/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, createDonation); // Rota para criar uma doação
+donationRoutes.get('/donations/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, readDonation); // Rota para ler uma doação específica
+donationRoutes.patch('/donations/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, addBloodExams); // Rota para adicionar exames de sangue
+donationRoutes.delete('/donations/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, deleteDonation); // Rota para deletar a última doação
 
 export default donationRoutes;
--- src/routes/donorRoutes.js
+++ src/routes/donorRoutes.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { createDonor, getDonor, updateDonor, deleteDonor } from '../controllers/DonorController.js';
-import { verifyEmployee } from '../middleware/VerifyEmployees.js';
+import { verifyAdmin, verifyEmployee } from '../middleware/VerifyEmployees.js';
 import { checkBlacklistedToken } from '../middleware/Blacklist.js';
 
 const donorRoutes = Router();
 
-donorRoutes.post('/donors/', verifyEmployee, checkBlacklistedToken, createDonor); // Rota para criar um doador
-donorRoutes.get('/donors/:id?', verifyEmployee, checkBlacklistedToken, getDonor); // O id é opcional
-donorRoutes.get('/donors/:name?', verifyEmployee, checkBlacklistedToken, getDonor); // O nome é opcional
-donorRoutes.patch('/donors/:id', verifyEmployee, checkBlacklistedToken, updateDonor); // Rota para atualizar um doador
-donorRoutes.delete('/donors/:id', verifyEmployee, checkBlacklistedToken, deleteDonor); // Rota para deletar um doador
+donorRoutes.post('/donors/', verifyAdmin, verifyEmployee, checkBlacklistedToken, createDonor); // Rota para criar um doador
+donorRoutes.get('/donors/:id?', verifyAdmin, verifyEmployee, checkBlacklistedToken, getDonor); // O id é opcional
+donorRoutes.get('/donors/:name?', verifyAdmin, verifyEmployee, checkBlacklistedToken, getDonor); // O nome é opcional
+donorRoutes.patch('/donors/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, updateDonor); // Rota para atualizar um doador
+donorRoutes.delete('/donors/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, deleteDonor); // Rota para deletar um doador
 
 export default donorRoutes;
