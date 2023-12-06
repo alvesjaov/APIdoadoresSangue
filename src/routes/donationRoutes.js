759cd4380a3030b9039a38e061d4bc9aacb3c4f4
João Victor Alves <victoralves169@outlook.com>
2023-12-06 13:07:11 -0300

    Refactor: donationRoutes.js

--- src/routes/donationRoutes.js
+++ src/routes/donationRoutes.js
@@ -7,8 +7,7 @@ import { checkBlacklistedToken } from '../middleware/Blacklist.js';
 const donationRoutes = Router();
 
 donationRoutes.post('/donations/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, createDonation); // Rota para criar uma doação
-donationRoutes.get('/donations', verifyAdmin, verifyEmployee, checkBlacklistedToken, readAllDonations);
-//rota para retornar todas doações
+donationRoutes.get('/donations', verifyAdmin, verifyEmployee, checkBlacklistedToken, readAllDonations); //rota para retornar todas doações
 donationRoutes.get('/donations/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, readDonation); // Rota para ler uma doação específica
 donationRoutes.patch('/donations/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, addBloodExams); // Rota para adicionar exames de sangue
 donationRoutes.delete('/donations/:id', verifyAdmin, verifyEmployee, checkBlacklistedToken, deleteDonation); // Rota para deletar a última doação
